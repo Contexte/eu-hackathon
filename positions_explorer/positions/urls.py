c16981bc1409c5a5b265db05e5d089d69e41c1e2
Anthony Delaby <adelaby@contexte.com>
2014-12-02 19:02:39 +0100

    renamed url pattern

--- positions_explorer/positions/urls.py
+++ positions_explorer/positions/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.ContributorsList.as_view(), name='contributors-list'),
-    url(r'^axis/((?P<pk>\d+)/)?$', views.AxisDetail.as_view(), name='contributor-detail'),
+    url(r'^axis/((?P<pk>\d+)/)?$', views.AxisDetail.as_view(), name='axis-detail'),
 )
