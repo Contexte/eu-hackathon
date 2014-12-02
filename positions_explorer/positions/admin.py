13715ad16854613d805a69f7acd01ff0a4f96402
Anthony Delaby <adelaby@contexte.com>
2014-12-02 14:13:09 +0100

    added basic admin interface

--- positions_explorer/positions/admin.py
+++ positions_explorer/positions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    pass
+
+
+class AxisAdmin(admin.ModelAdmin):
+    pass
+
+
+class AxisValuesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Contributor, ContributorAdmin)
+admin.site.register(models.Axis, AxisAdmin)
+admin.site.register(models.AxisValues, AxisValuesAdmin)
