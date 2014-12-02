20bd0ce5bcb76f75e522760767a5a8b48d5943e6
Anthony Delaby <adelaby@contexte.com>
2014-12-02 23:45:54 +0100

    added contribution values form

---
+++ positions_explorer/positions/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from . import models
+
+
+class AxisValuesForm(forms.Form):
+
+    values = forms.ModelMultipleChoiceField(
+        models.AxisValues.objects.none(),
+        to_field_name='value'
+    )
+    contributor = forms.ModelChoiceField(
+        models.Contributor.objects.none(),
+        widget=forms.HiddenInput
+    )
+
+    def __init__(self, *args, **kwargs):
+        axis = kwargs.pop('axis')
+        contributor_pk = kwargs.pop('contributor_pk', None)
+        super(AxisValuesForm, self).__init__(*args, **kwargs)
+        self.fields['values'].queryset = models.AxisValues.objects.filter(axis=axis)
+        if contributor_pk:
+            self.fields['contributor'].queryset = models.Contributor.objects.filter(pk=contributor_pk)
+            self.fields['contributor'].initial = self.fields['contributor'].queryset.first()
