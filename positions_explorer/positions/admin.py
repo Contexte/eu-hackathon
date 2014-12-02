23c14172e799feeedb0da9c7a11514767f4f7dd5
Anthony Delaby <adelaby@contexte.com>
2014-12-02 15:12:34 +0100

    use inline model admin

--- positions_explorer/positions/admin.py
+++ positions_explorer/positions/admin.py
@@ -3,17 +3,20 @@ from django.contrib import admin
 from . import models
 
 
+class AxisValuesAdmin(admin.TabularInline):
+    model = models.AxisValues
+
+
+class AxisAdmin(admin.ModelAdmin):
+    inlines = (
+        AxisValuesAdmin,
+    )
+
+
 class ContributorAdmin(admin.ModelAdmin):
     pass
 
 
-class AxisAdmin(admin.ModelAdmin):
-    pass
-
-
-class AxisValuesAdmin(admin.ModelAdmin):
-    pass
-
+admin.site.register(models.AxisValues)
 admin.site.register(models.Contributor, ContributorAdmin)
 admin.site.register(models.Axis, AxisAdmin)
-admin.site.register(models.AxisValues, AxisValuesAdmin)
