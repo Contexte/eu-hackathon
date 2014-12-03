c173c6c3f50ef2e01e12b3a13c9e50349a1438b5
Anthony Delaby <adelaby@contexte.com>
2014-12-03 09:49:20 +0100

    fixed axis none

--- positions_explorer/positions/managers.py
+++ positions_explorer/positions/managers.py
@@ -9,7 +9,7 @@ class AxisManager(models.Manager):
     def get_random_axis(self, languages=[]):
         axis = get_random_object_from_queryset(self)
         rejected = []
-        while axis.is_fully_qualified and axis.has_contributor_for_languages(languages):
+        while axis and axis.is_fully_qualified and axis.has_contributor_for_languages(languages):
             rejected.append(axis.pk)
             axis = get_random_object_from_queryset(self.exclude(pk__in=rejected))
         return axis
